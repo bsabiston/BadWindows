e40c00720b2047f7394742497bc8cdd0cf5678c3
bsabiston <bob@flatblackfilms.com>
2018-03-22 18:16:03 -0500

    second commit

---
+++ src/libcolor/munsell.c
@@ -0,0 +1,1 @@
+#define NUMH 40 /* 0 to 100 by 2.5 */#define NUMV 12  /* 1 to 12 */  /* values 10 - 12 are extrapolated */#define NUMC 15 /* 0 to 26 by 2 */float   munciedata[NUMV][NUMH][NUMC][2];char    mundata[] = "/u/walter/ncolor/munsell2cie.data";int     munsellinit = 0;
